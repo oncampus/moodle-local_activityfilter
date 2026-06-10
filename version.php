fd2f618bd69f449d065f93ae73b0dedb85380871
Konrad Ebel <konrad.ebel@oncampus.de>
2026-06-10 18:50:45 +0200

    Bump version number to v2.1.0

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_activityfilter';
-$plugin->release = '2.0.0';
-$plugin->version = 2026060900;
+$plugin->release = '2.1.0';
+$plugin->version = 2026061000;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
